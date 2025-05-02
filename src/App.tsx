7f187c7483944b9b656f9a1e047b0d0c89924995
Ahmkwj <Ahmkwj@gmail.com>
2025-05-02 13:53:41 +0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="min-h-screen flex flex-col items-center justify-center p-8 text-center">
+      <div className="flex justify-center mb-8">
+        <a href="https://vite.dev" target="_blank" className="mx-4">
+          <img
+            src={viteLogo}
+            className="h-24 w-auto transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" className="mx-4">
+          <img
+            src={reactLogo}
+            className="h-24 w-auto transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa]"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-4xl font-bold tex mb-8">Vite + React</h1>
+      <div className="p-8 bg-white/5 rounded-lg shadow-md mb-8">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-slate-800 hover:bg-slate-700 text-white font-medium py-2 px-4 rounded-md transition-colors mb-4"
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className="mt-4">
+          Edit{" "}
+          <code className="font-mono bg-slate-800/20 p-1 rounded">
+            src/App.tsx
+          </code>{" "}
+          and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-500">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
