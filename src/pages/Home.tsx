402e7969d7b0425efe653717e8aa8cf41e078642
Ahmkwj <Ahmkwj@gmail.com>
2025-05-02 13:23:19 +0300

    ready to go

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-page">
+      <h1>UDST Tools</h1>
+      <p>Welcome to the UDST student tools website.</p>
+    </div>
+  );
+};
+
+export default Home;
